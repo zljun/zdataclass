0c712458470c2b0b0aff7c1bf2b571992967d9e4
zljun <zuolongjun@163.com>
2019-04-29 17:11:24 +0800

    fix some issues on bitfields

--- zdataclass.py
+++ zdataclass.py
@@ -1,3 +1,4 @@
+from enum import IntEnum, IntFlag
 from dataclasses import dataclass
 import dataclasses
 from binascii import hexlify
@@ -9,6 +10,11 @@ try:
     logger_zdataclass = logging.getLogger(__name__)
 except:
     logger_zdataclass = None
+TRACE_LEVEL_NONE = 0
+TRACE_LEVEL_DEBUG = 1
+TRACE_LEVEL_INFO = 2
+TRACE_LEVEL_WARN = 3
+TRACE_LEVEL_ERROR = 4
 
 # see example s_with_auto_field
 LENGTH_FIELD = 'length' 
@@ -129,10 +135,71 @@ class uint3(base_int):
 class uint4(base_int):
     W = 4/8
     ENDIAN = 'little'
+    
+class uint5(base_int):
+    W = 5/8
+    ENDIAN = 'little'
+
+class uint6(base_int):
+    W = 6/8
+    ENDIAN = 'little'
+
+class uint7(base_int):
+    W = 7/8
+    ENDIAN = 'little'
+
+class uint9(base_int):
+    W = 9/8
+    ENDIAN = 'little'
+
+class uint10(base_int):
+    W = 10/8
+    ENDIAN = 'little'
+
+class uint11(base_int):
+    W = 11/8
+    ENDIAN = 'little'
 
 class uint12(base_int):
     W = 12/8
     ENDIAN = 'little'
+
+class uint13(base_int):
+    W = 13/8
+    ENDIAN = 'little'
+
+class uint14(base_int):
+    W = 14/8
+    ENDIAN = 'little'
+
+class uint15(base_int):
+    W = 15/8
+    ENDIAN = 'little'
+    
+class IntEnum8(IntEnum):    
+    def __len__(self):
+        return 1
+    def is_member(self, value):
+        for x in self.__members__:
+            if self.__members__[x] == value:
+                return True
+        return False
+
+class IntEnum16(IntEnum):    
+    def __len__(self):
+        return 2
+        
+class IntFlag8(IntFlag):    
+    def __len__(self):
+        return 1
+
+class IntFlag16(IntFlag):    
+    def __len__(self):
+        return 2
+
+class IntFlag32(IntFlag):    
+    def __len__(self):
+        return 4
 
 class int_array():
     W = None
@@ -210,16 +277,186 @@ class uint16_array(int_array):
 class uint32_array(int_array):   
     W=4
     ENDIAN = 'little'
+	
+class type_len_data_t():
+    def __len__(self): # including size field
+        return self.real_size + self.offset
+
+    def unpack(self, data, n, mask=None): 
+        if len(data) <abs(n):
+            logger_cotypes.warn('int_array len(data)={} <{}'.format(len(data), abs(n)))
+            return self
+        if n <0:
+            self.size = int.from_bytes(data[0:abs(n)], self.ENDIAN)
+            self.offset = abs(n)
+        else:
+            self.size = n * self.W
+            self.offset = 0
+        if mask == None:
+            self.real_size = self.size
+        else:
+            self.real_size = self.size & mask
+        self.n_items = int(self.real_size / self.W)
+        self.array = list()
+        W = self.W 
+        if W*self.n_items + self.offset>len(data):
+            logger_cotypes.warn('int_array not enough data: n_items={}'.format(self.n_items))
+            return self 
+        for i in range(self.n_items):
+            idx = W*i + self.offset
+            self.array.append(int.from_bytes(data[idx: idx+W], self.ENDIAN))
+        return self
+
+# sdp data element
+# byte[0].bit[7:3] element type
+class data_element_type(IntEnum):
+    NULL = 0
+    UINT = 1
+    SINT = 2
+    UUID = 3
+    STRING = 4
+    BOOL = 5
+    DATA_ELEMENT_SEQ = 6
+    DATA_ELEMENT_ALT = 7
+    URL = 8
+
+# sdp data element
+# byte[0].bit[2:0] element size descriptor
+class data_element_size_t(IntEnum):
+    ONE = 0
+    TWO = 1
+    FOUR = 2
+    EIGHT = 3
+    SIXTEEN = 4
+    IN_NEXT_U8 = 5
+    IN_NEXT_U16 = 6
+    IN_NEXT_U32 = 7  
+    
+class sdp_data_element_t():
+    def __init__(self, element_type=None, element_size=None, element_data=None):
+        self.element_type = element_type
+        if element_size !=None:
+            self.set_element_size(element_size)
+        self.element_data = element_data
+
+    def set_element_size(self, element_size):
+        self.element_size = element_size
+        if element_size == 1:
+            self.element_size_desc = data_element_size_t.ONE
+        elif element_size == 2:
+            self.element_size_desc = data_element_size_t.TWO
+        elif element_size == 4:
+            self.element_size_desc = data_element_size_t.FOUR
+        elif element_size == 8:
+            self.element_size_desc = data_element_size_t.EIGHT
+        elif element_size == 16:
+            self.element_size_desc = data_element_size_t.SIXTEEN
+        elif element_size <256:
+            self.element_size_desc = data_element_size_t.IN_NEXT_U8
+        elif element_size <65536:
+            self.element_size_desc = data_element_size_t.IN_NEXT_U16
+        else:
+            self.element_size_desc = data_element_size_t.IN_NEXT_U32
+
+    def to_bytes(self):
+        if self.element_type == data_element_type.NULL:
+            return (self.element_type<<5).to_bytes(1, 'big')
+            
+        if self.element_type == None or self.element_size == None or self.element_data == None:
+            return b''
+            
+        data = (((self.element_type&0x1F)<<3) | (self.element_size_desc&0x07)).to_bytes(1, 'big')
+        if self.element_size_desc == data_element_size_t.IN_NEXT_U8:
+            data += self.element_size.to_bytes(1, 'big')
+        elif self.element_size_desc == data_element_size_t.IN_NEXT_U16:
+            data += self.element_size.to_bytes(2, 'big')  
+        elif self.element_size_desc == data_element_size_t.IN_NEXT_U32:
+            data += self.element_size.to_bytes(4, 'big')
+        if self.element_type in [data_element_type.UINT, data_element_type.SINT, data_element_type.BOOL]:
+            W = 1 <<self.element_size_desc
+            data += self.element_data.to_bytes(W, 'big')
+        elif self.element_type in [data_element_type.UUID, data_element_type.STRING]:
+            data += bytes(self.element_data)
+        elif self.element_type in [data_element_type.DATA_ELEMENT_SEQ, data_element_type.DATA_ELEMENT_ALT]:
+            for ele in self.element_data:
+                data += ele.to_bytes()
+        else:
+            logger_cotypes.error('Unknown element type {}'.format(self.element_type))
+        
+        return data
+
+    def from_bytes(self, data):
+        if len(data) == 0:
+            self.element_type = data_element_type.NULL
+            self.element_size_desc = 0
+            self.element_size = 0
+            self.element_data = b''
+            return
+        try:
+            self.element_type = data_element_type(data[0] >>3)
+        except:
+            self.element_type = data[0] >>3
+        self.element_size_desc = data_element_size_t(data[0] & 0x07)
+        if self.element_size_desc <= data_element_size_t.SIXTEEN:
+            self.element_size = 1 <<self.element_size_desc
+            offset = 1
+        elif self.element_size_desc == data_element_size_t.IN_NEXT_U8:
+            self.element_size = data[1]
+            offset = 2
+        elif self.element_size_desc == data_element_size_t.IN_NEXT_U16:
+            self.element_size = int.from_bytes(data[1:3], 'big')
+            offset = 3
+        elif self.element_size_desc == data_element_size_t.IN_NEXT_U32:
+            self.element_size = int.from_bytes(data[1:5], 'big')
+            offset = 5
+            
+        if self.element_type == data_element_type.NULL:
+            self.element_data = None
+        elif self.element_type == data_element_type.UINT:
+            self.element_data = int.from_bytes(data[offset:offset+self.element_size], 'big')
+        elif self.element_type == data_element_type.SINT:
+            self.element_data = int.from_bytes(data[offset:offset+self.element_size], 'big', signed=True)
+        elif self.element_type == data_element_type.UUID:
+            self.element_data = data[offset:offset+self.element_size]
+        elif self.element_type == data_element_type.DATA_ELEMENT_SEQ:
+            count = self.element_size
+            self.element_data = []
+            while count >0:
+                ele = sdp_data_element_t().from_bytes(data[offset:])
+                offset += len(ele)
+                count -= len(ele)
+                self.element_data.append(ele)
+        else:
+            self.element_data = data[offset:offset+self.element_size]
+        return self
+                
+    def __len__(self):
+        if self.element_size_desc <= data_element_size_t.SIXTEEN:
+            offset = 1
+        elif self.element_size_desc == data_element_size_t.IN_NEXT_U8:
+            offset = 2
+        elif self.element_size_desc == data_element_size_t.IN_NEXT_U16:
+            offset = 3
+        elif self.element_size_desc == data_element_size_t.IN_NEXT_U32:
+            offset = 5
+        return self.element_size + offset
+        
+    def __repr__(self):
+        format_str = 'data element {}: size={}'.format(repr(self.element_type), self.element_size)
+        if True: #type(self.element_data) != list:
+            format_str += ', data={}'.format(self.element_data)
+            self.repr = format_str
+            return format_str
+        format_str += ', data:\r\n'
+        for d in self.element_data:
+            format_str += '\r\n    {}'.format(d)
+        self.repr = format_str
+        return self.repr        
 
 @dataclass
-class basedataclass:
-    TRACE_LEVEL_NONE = 0
-    TRACE_LEVEL_DEBUG = 1
-    TRACE_LEVEL_INFO = 2
-    TRACE_LEVEL_WARN = 3
-    TRACE_LEVEL_ERROR = 4
+class basedataclass:    
     def __post_init__(self):
-        self.trace_level = self.TRACE_LEVEL_NONE
+        self.trace_level = TRACE_LEVEL_NONE
         # Deal with special fields from tail to head. A field is union and <length, value> type 
         for x in dataclasses.fields(self)[::-1]: 
             fieldname = getattr(x, 'name')
@@ -229,6 +466,8 @@ class basedataclass:
                 setattr(self, fieldname, len(getattr(self,data_field)))
 
             # A union field shall be bytearray and be packed with its following fields
+            self.bit_offset = 0
+            self.bitfields = []
             if self.is_union_field(x):
                 found = False
                 m = b''
@@ -238,7 +477,18 @@ class basedataclass:
                         continue
                     if found == True:
                         try:
-                            m += self.pack_field(y)
+                            fieldtype = getattr(y, 'type')
+                            if fieldtype in base_int.__subclasses__() and fieldtype.W !=int(fieldtype.W):
+                                fieldname1 = getattr(y, 'name')
+                                value = getattr(self, fieldname1)
+                                self.bit_offset += int(fieldtype.W * 8)
+                                self.bitfields.append((int(fieldtype.W * 8), value))
+                                if self.bit_offset & 7 == 0:
+                                    m += self.pack_bitfields(self.bitfields)
+                                    self.bit_offset = 0
+                                    self.bitfields = []
+                            else:
+                                m += self.pack_field(y)
                         except Exception as e:
                             pass
                 if len(m):
@@ -342,7 +592,8 @@ class basedataclass:
         if m != None:
             return m  
                  
-        raise Exception('Unknown length for field {}'.format(x))
+        #raise Exception('Unknown length for field {}'.format(x))
+        return 0
 
     def __len__(self):
         n = 0
@@ -403,13 +654,19 @@ class basedataclass:
     def debug(self, s):
         if logger_zdataclass != None:
             logger_zdataclass.debug(s)
-        elif self.trace_level >=self.TRACE_LEVEL_DEBUG:
+        elif self.trace_level >=TRACE_LEVEL_DEBUG:
             print(s)
             
     def info(self, s):
         if logger_zdataclass != None:
             logger_zdataclass.info(s)
-        elif self.trace_level >=self.TRACE_LEVEL_INFO:
+        elif self.trace_level >=TRACE_LEVEL_INFO:
+            print(s)
+			
+    def warn(self, s):
+        if logger_zdataclass != None:
+            logger_zdataclass.info(s)
+        elif self.trace_level >=TRACE_LEVEL_INFO:
             print(s)
               
     def pack(self):
@@ -460,10 +717,15 @@ class basedataclass:
 
             if t in base_int.__subclasses__() and t.W !=int(t.W):
                 self.bit_offset += int(t.W * 8)
-                self.bitfields.append((int(t.W * 8), fieldname))
+                self.bitfields.append((int(t.W * 8), fieldname, default))
                 if self.bit_offset & 7 == 0:
-                    
                     L = self.unpack_bitfields(self.bitfields, data[offset:] )
+                    for bitfield in self.bitfields:
+                        value = getattr(self, bitfield[1])
+                        default = bitfield[2]
+                        if default != None and default != value and type(default) !=dataclasses._MISSING_TYPE:
+                            return None
+        
                     self.bit_offset = 0
                     self.bitfields = []
             elif t == str:
@@ -472,7 +734,11 @@ class basedataclass:
                 value = data[offset:offset+L]
             else:
                 try:
-                    value = t.from_bytes(data[offset:offset+L])
+                    if t != sdp_data_element_t:
+                        value = t.from_bytes(data[offset:offset+L])
+                    else:
+                        value = sdp_data_element_t().from_bytes(data[offset:])
+                        L = len(value)
                 except Exception as e:
                     print(e)
                     raise e
@@ -480,12 +746,12 @@ class basedataclass:
             if self.is_union_field(x) == False and int(L) == L:
                 offset += L
 
+            if t in base_int.__subclasses__() and t.W !=int(t.W):                
+                continue
+
             # field with default value shall be same as unpacked value
             if default != None and default != value and type(default) !=dataclasses._MISSING_TYPE:
                 return None
-
-            if t in base_int.__subclasses__() and t.W !=int(t.W):
-                continue
             
             # type convert
             if type(value) != t:
@@ -550,6 +816,7 @@ class s_with_int_array(basedataclass):
 
 @dataclass
 class s_with_bitfield(basedataclass):
+    data: bytearray = dataclasses.field(default_factory=bytearray, metadata={LENGTH_FIELD:4, UNION_FIELD:True})
     head: uint8 = None
     handle:  uint12 = None
     pb_flag: uint2 = None
